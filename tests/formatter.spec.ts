7f93e1e6279716ca35bbc27840bacd8d7939af23
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-30 12:54:29 +0300

    Make all test data plural

--- tests/formatter.spec.ts
+++ tests/formatter.spec.ts
@@ -2,26 +2,26 @@ import {Formatter} from '../src/formatter';
 
 describe('formatter', () => {
     it('shows seconds when when the input is less than a minute', () => {
-        const formattedValue = new Formatter().format(4);
+        const formattedValue = new Formatter().format(5);
 
-        expect(formattedValue).toEqual('0 days, 0 hours, 0 minutes 4 seconds');
+        expect(formattedValue).toEqual('0 days, 0 hours, 0 minutes 5 seconds');
     });
 
     it('shows minutes and seconds when the input is greater than a minute', () => {
-        const formattedValue = new Formatter().format(3 * 60 + 4);
+        const formattedValue = new Formatter().format(4 * 60 + 5);
 
-        expect(formattedValue).toEqual('0 days, 0 hours, 3 minutes 4 seconds');
+        expect(formattedValue).toEqual('0 days, 0 hours, 4 minutes 5 seconds');
     });
 
     it('shows hours, minutes and seconds when the input is greater than an hour', () => {
-        const formattedValue = new Formatter().format(2 * 60 * 60 + 3 * 60 + 4);
+        const formattedValue = new Formatter().format(3 * 60 * 60 + 4 * 60 + 5);
 
-        expect(formattedValue).toEqual('0 days, 2 hours, 3 minutes 4 seconds');
+        expect(formattedValue).toEqual('0 days, 3 hours, 4 minutes 5 seconds');
     });
 
     it('shows days, hours, minutes and seconds when the input is greater than a day', () => {
-        const formattedValue = new Formatter().format(24 * 60 * 60 + 2 * 60 * 60 + 3 * 60 + 4);
+        const formattedValue = new Formatter().format(2 * 24 * 60 * 60 + 3 * 60 * 60 + 4 * 60 + 5);
 
-        expect(formattedValue).toEqual('1 days, 2 hours, 3 minutes 4 seconds');
+        expect(formattedValue).toEqual('2 days, 3 hours, 4 minutes 5 seconds');
     });
 });
