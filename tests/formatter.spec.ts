487ab73c380a0c42b7ee4efb90eb1e7ab03898bc
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-30 12:50:32 +0300

    Make test data a bit more consistent

--- tests/formatter.spec.ts
+++ tests/formatter.spec.ts
@@ -8,21 +8,21 @@ describe('formatter', () => {
     });
 
     it('shows seconds when when the input is less than a minute', () => {
-        const formattedValue = new Formatter().format(1);
+        const formattedValue = new Formatter().format(4);
 
-        expect(formattedValue).toEqual('0 days, 0 hours, 0 minutes 1 seconds');
+        expect(formattedValue).toEqual('0 days, 0 hours, 0 minutes 4 seconds');
     });
 
     it('shows minutes and seconds when the input is greater than a minute', () => {
-        const formattedValue = new Formatter().format(62);
+        const formattedValue = new Formatter().format(3 * 60 + 4);
 
-        expect(formattedValue).toEqual('0 days, 0 hours, 1 minutes 2 seconds');
+        expect(formattedValue).toEqual('0 days, 0 hours, 3 minutes 4 seconds');
     });
 
     it('shows hours, minutes and seconds when the input is greater than an hour', () => {
-        const formattedValue = new Formatter().format(60 * 60 + 2 * 60 + 3);
+        const formattedValue = new Formatter().format(2 * 60 * 60 + 3 * 60 + 4);
 
-        expect(formattedValue).toEqual('0 days, 1 hours, 2 minutes 3 seconds');
+        expect(formattedValue).toEqual('0 days, 2 hours, 3 minutes 4 seconds');
     });
 
     it('shows days, hours, minutes and seconds when the input is greater than a day', () => {
