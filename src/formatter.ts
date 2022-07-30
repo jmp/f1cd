8a765dff58dfdd80a513cc0ef6b31ee8be943d03
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-30 14:26:19 +0300

    Inline `formatUnit`

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,17 +1,15 @@
 export class Formatter {
     format(totalSeconds: number): string {
-        const formatUnit = (value: number, unit: string) => value === 1 ? unit : `${unit}s`;
-
         const days = Math.floor(totalSeconds / (3600 * 24));
         const hours = Math.floor(totalSeconds % (3600 * 24) / 3600);
         const minutes = Math.floor(totalSeconds % 3600 / 60);
         const seconds = Math.floor(totalSeconds % 60);
 
         return [
-            days, formatUnit(days, 'day'),
-            hours, formatUnit(hours, 'hour'),
-            minutes, formatUnit(minutes, 'minute'),
-            seconds, formatUnit(seconds, 'second'),
+            days, days === 1 ? 'day' : 'days',
+            hours, hours === 1 ? 'hour' : 'hours',
+            minutes, minutes === 1 ? 'minute' : 'minutes',
+            seconds, seconds === 1 ? 'second' : 'seconds'
         ].join(' ');
     }
 }
