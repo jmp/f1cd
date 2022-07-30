0d4537cc8e2246becd77114f61b86464508fd533
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-30 13:22:36 +0300

    Show singular or plural depending on the value

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,9 +1,17 @@
 export class Formatter {
     format(totalSeconds: number): string {
+        const formatUnit = (value: number, unit: string) => value === 1 ? unit : `${unit}s`;
+
         const days = Math.floor(totalSeconds / (3600 * 24));
         const hours = Math.floor(totalSeconds % (3600 * 24) / 3600);
         const minutes = Math.floor(totalSeconds % 3600 / 60);
         const seconds = Math.floor(totalSeconds % 60);
-        return `${days} days, ${hours} hours, ${minutes} minutes ${seconds} seconds`;
+
+        return [
+            days, formatUnit(days, 'day'),
+            hours, formatUnit(hours, 'hour'),
+            minutes, formatUnit(minutes, 'minute'),
+            seconds, formatUnit(seconds, 'second'),
+        ].join(' ');
     }
 }
--- tests/formatter.spec.ts
+++ tests/formatter.spec.ts
@@ -4,24 +4,48 @@ describe('formatter', () => {
     it('shows seconds when when the input is less than a minute', () => {
         const formattedValue = new Formatter().format(5);
 
-        expect(formattedValue).toEqual('0 days, 0 hours, 0 minutes 5 seconds');
+        expect(formattedValue).toEqual('0 days 0 hours 0 minutes 5 seconds');
     });
 
     it('shows minutes and seconds when the input is greater than a minute', () => {
         const formattedValue = new Formatter().format(4 * 60 + 5);
 
-        expect(formattedValue).toEqual('0 days, 0 hours, 4 minutes 5 seconds');
+        expect(formattedValue).toEqual('0 days 0 hours 4 minutes 5 seconds');
     });
 
     it('shows hours, minutes and seconds when the input is greater than an hour', () => {
         const formattedValue = new Formatter().format(3 * 60 * 60 + 4 * 60 + 5);
 
-        expect(formattedValue).toEqual('0 days, 3 hours, 4 minutes 5 seconds');
+        expect(formattedValue).toEqual('0 days 3 hours 4 minutes 5 seconds');
     });
 
     it('shows days, hours, minutes and seconds when the input is greater than a day', () => {
         const formattedValue = new Formatter().format(2 * 24 * 60 * 60 + 3 * 60 * 60 + 4 * 60 + 5);
 
-        expect(formattedValue).toEqual('2 days, 3 hours, 4 minutes 5 seconds');
+        expect(formattedValue).toEqual('2 days 3 hours 4 minutes 5 seconds');
+    });
+
+    it('shows seconds in singular when when there is exactly one second', () => {
+        const formattedValue = new Formatter().format(1);
+
+        expect(formattedValue).toEqual('0 days 0 hours 0 minutes 1 second');
+    });
+
+    it('shows minutes in singular when when there is exactly one minute', () => {
+        const formattedValue = new Formatter().format(60);
+
+        expect(formattedValue).toEqual('0 days 0 hours 1 minute 0 seconds');
+    });
+
+    it('shows hours in singular when when there is exactly one hour', () => {
+        const formattedValue = new Formatter().format(60 * 60);
+
+        expect(formattedValue).toEqual('0 days 1 hour 0 minutes 0 seconds');
+    });
+
+    it('shows days in singular when when there is exactly one day', () => {
+        const formattedValue = new Formatter().format(24 * 60 * 60);
+
+        expect(formattedValue).toEqual('1 day 0 hours 0 minutes 0 seconds');
     });
 });
