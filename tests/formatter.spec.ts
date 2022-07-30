afe5e765be5ec30b1d6259f489acc2711d95f4c3
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-30 12:03:57 +0300

    Fix test name

--- tests/formatter.spec.ts
+++ tests/formatter.spec.ts
@@ -1,7 +1,7 @@
 import {Formatter} from '../src/formatter';
 
 describe('formatter', () => {
-    it('is zero when the dates are the same', () => {
+    it('shows zero for days, hours, minutes and seconds when the input is zero', () => {
         const formattedValue = new Formatter().format(0);
 
         expect(formattedValue).toEqual('0 days, 0 hours, 0 minutes and 0 seconds');
