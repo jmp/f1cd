a144fdbc8ad175a9a2f2f81ba7afb1ece8e93c90
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-30 20:25:38 +0300

    Count down to the next session

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import {Countdown} from './countdown';
 import {Formatter} from './formatter';
+import {SessionFinder} from './session-finder';
+import sessionData from './data/sessions.json';
 
 const targetId = 'countdown';
 const updateInterval = 1000;
 
 const formatter = new Formatter();
+const sessions = sessionData.map(({ title, date }) => ({ title, date: new Date(date) }));
+const sessionFinder = new SessionFinder(sessions);
 const targetElement = document.getElementById(targetId)!;
 
 function updateCountdown() {
-    const countdown = new Countdown(new Date(), new Date('2022-12-24'), formatter);
+    const now = new Date();
+    const session = sessionFinder.findNext(now);
+    const countdown = new Countdown(now, session!.date, formatter);
     targetElement.innerHTML = countdown.format();
 }
 
