462446f1088f02d8fc9271c607002862d8255405
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-30 21:10:33 +0300

    Inline getting the elements

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,17 @@ import {Formatter} from './formatter';
 import {SessionFinder} from './session-finder';
 import sessions from './data/sessions.json';
 
-const updateInterval = 1000;
-
 const formatter = new Formatter();
 const sessionFinder = new SessionFinder(sessions);
-
-const roundElement = document.getElementById('round')!;
-const sessionElement = document.getElementById('session')!;
-const countdownElement = document.getElementById('countdown')!;
 
 function updateCountdown() {
     const now = new Date();
     const session = sessionFinder.findNext(now);
     const countdown = new Countdown(now, session!.date, formatter);
-    roundElement.innerHTML = session!.round;
-    sessionElement.innerHTML = session!.title;
-    countdownElement.innerHTML = countdown.format();
+    document.getElementById('round')!.innerHTML = session!.round;
+    document.getElementById('session')!.innerHTML = session!.title;
+    document.getElementById('countdown')!.innerHTML = countdown.format();
 }
 
 updateCountdown();
-setInterval(() => updateCountdown(), updateInterval);
+setInterval(() => updateCountdown(), 1000);
