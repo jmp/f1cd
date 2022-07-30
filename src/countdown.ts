8533b286b9174075fff257bb7cbb850b67c36789
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-30 14:17:51 +0300

    Make `Countdown` use `Formatter`

--- src/countdown.ts
+++ src/countdown.ts
@@ -1,7 +1,15 @@
+import {Formatter} from './formatter';
+
 export class Countdown {
     readonly seconds: number;
+    private readonly formatter: Formatter;
 
-    constructor(from: Date, to: Date) {
+    constructor(from: Date, to: Date, formatter: Formatter) {
         this.seconds = Math.max(0, to.getTime() - from.getTime()) / 1000;
+        this.formatter = formatter;
+    }
+
+    format(): string {
+        return this.formatter.format(this.seconds);
     }
 }
--- tests/countdown.spec.ts
+++ tests/countdown.spec.ts
@@ -1,10 +1,16 @@
 import {Countdown} from '../src/countdown';
+import {Formatter} from '../src/formatter';
 
 describe('countdown', () => {
+    const dummyFormatter: Formatter = {
+        format: () => ''
+    }
+
     it('is zero when the dates are the same', () => {
         const countdown = new Countdown(
             new Date('2022-01-01T00:00:00Z'),
-            new Date('2022-01-01T00:00:00Z')
+            new Date('2022-01-01T00:00:00Z'),
+            dummyFormatter
         );
         expect(countdown.seconds).toEqual(0);
     });
@@ -12,7 +18,8 @@ describe('countdown', () => {
     it('is zero when the "to" date is before "from" date', () => {
         const countdown = new Countdown(
             new Date('2022-01-01T00:00:30Z'),
-            new Date('2022-01-01T00:00:00Z')
+            new Date('2022-01-01T00:00:00Z'),
+            dummyFormatter
         );
         expect(countdown.seconds).toEqual(0);
     });
@@ -20,8 +27,22 @@ describe('countdown', () => {
     it('is the difference in seconds when the dates are different', () => {
         const countdown = new Countdown(
             new Date('2022-01-01T00:00:00Z'),
-            new Date('2022-01-01T00:00:42Z')
+            new Date('2022-01-01T00:00:42Z'),
+            dummyFormatter
         );
         expect(countdown.seconds).toEqual(42);
+    });
+
+    it('formats the time using the given formatter', () => {
+        const formatter: Formatter = {
+            format: (seconds: number) => `formatted ${seconds}`
+        }
+        const countdown = new Countdown(
+            new Date('2022-01-01T00:00:00Z'),
+            new Date('2022-01-01T00:00:42Z'),
+            formatter
+        );
+
+        expect(countdown.format()).toEqual('formatted 42');
     });
 });
