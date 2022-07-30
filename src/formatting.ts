d3ae4329512d46e5dd16d8eb523c8539b8ba8260
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-30 13:51:08 +0300

    Make `format` into a function

--- src/formatting.ts
+++ src/formatting.ts
@@ -1,17 +1,15 @@
-export class Formatter {
-    format(totalSeconds: number): string {
-        const formatUnit = (value: number, unit: string) => value === 1 ? unit : `${unit}s`;
+export function format(totalSeconds: number) {
+    const formatUnit = (value: number, unit: string) => value === 1 ? unit : `${unit}s`;
 
-        const days = Math.floor(totalSeconds / (3600 * 24));
-        const hours = Math.floor(totalSeconds % (3600 * 24) / 3600);
-        const minutes = Math.floor(totalSeconds % 3600 / 60);
-        const seconds = Math.floor(totalSeconds % 60);
+    const days = Math.floor(totalSeconds / (3600 * 24));
+    const hours = Math.floor(totalSeconds % (3600 * 24) / 3600);
+    const minutes = Math.floor(totalSeconds % 3600 / 60);
+    const seconds = Math.floor(totalSeconds % 60);
 
-        return [
-            days, formatUnit(days, 'day'),
-            hours, formatUnit(hours, 'hour'),
-            minutes, formatUnit(minutes, 'minute'),
-            seconds, formatUnit(seconds, 'second'),
-        ].join(' ');
-    }
+    return [
+        days, formatUnit(days, 'day'),
+        hours, formatUnit(hours, 'hour'),
+        minutes, formatUnit(minutes, 'minute'),
+        seconds, formatUnit(seconds, 'second'),
+    ].join(' ');
 }
