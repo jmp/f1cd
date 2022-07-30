26d58807ced43d2855b33370b96b145672cedb01
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-30 12:26:25 +0300

    Simplify calculations for formatting

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,9 +1,9 @@
 export class Formatter {
     format(totalSeconds: number): string {
-        const days = Math.floor(totalSeconds / (24 * 3600));
-        const hours = Math.floor((totalSeconds - days * 24 * 3600) / 3600);
-        const minutes = Math.floor((totalSeconds - days * 24 * 3600 - hours * 3600) / 60);
-        const seconds = totalSeconds - days * 24 * 3600 - hours * 3600 - minutes * 60;
+        const days = Math.floor(totalSeconds / (3600 * 24));
+        const hours = Math.floor(totalSeconds % (3600 * 24) / 3600);
+        const minutes = Math.floor(totalSeconds % 3600 / 60);
+        const seconds = Math.floor(totalSeconds % 60);
         return `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
     }
 }
