efce04ba2766110366fa8b45bae8691a31a0f97c
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-07-31 14:37:05 +0300

    Simplify constructor

--- src/countdown.ts
+++ src/countdown.ts
@@ -2,11 +2,9 @@ import {Formatter} from './formatter';
 
 export class Countdown {
     private readonly seconds: number;
-    private readonly formatter: Formatter;
 
-    constructor(from: Date, to: Date, formatter: Formatter) {
+    constructor(from: Date, to: Date, private readonly formatter: Formatter) {
         this.seconds = Math.max(0, to.getTime() - from.getTime()) / 1000;
-        this.formatter = formatter;
     }
 
     format(): string {
