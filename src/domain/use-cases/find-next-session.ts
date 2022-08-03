afe2b96f4b2e23d4d331810fc55984c446b5691b
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-08-03 18:24:58 +0300

    Find the round inside the use case

--- src/domain/use-cases/find-next-session.ts
+++ src/domain/use-cases/find-next-session.ts
@@ -1,6 +1,6 @@
 import {Round} from '../round';
-import {RoundFinder} from '../round-finder';
 import {SessionFinder} from '../session-finder';
+import {Session} from '../session';
 
 export type NextSession = {
     roundTitle: string;
@@ -12,12 +12,18 @@ export class FindNextSession {
     constructor(private readonly rounds: Round[]) {}
 
     findNextSession(fromDate: Date): NextSession {
-        const round = new RoundFinder(this.rounds).findNext(fromDate);
+        const round = this.findNextRound(fromDate);
         const session = new SessionFinder(round.sessions).findNext(fromDate);
         return {
             roundTitle: round.title,
             sessionTitle: session.title,
             date: session.date
         };
+    }
+
+    private findNextRound(fromDate: Date): Round {
+        const nextSession = (session: Session) => fromDate.getTime() <= session.date.getTime();
+        const nextRound = (round: Round) => round.sessions.find(nextSession);
+        return this.rounds.find(nextRound) ?? this.rounds[this.rounds.length - 1];
     }
 }
