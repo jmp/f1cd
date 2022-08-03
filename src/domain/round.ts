cf1e0a9e0a784c213fc1593afef14c0461be4c41
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-08-03 18:07:26 +0300

    Add missing semicolons

--- src/domain/round.ts
+++ src/domain/round.ts
@@ -3,4 +3,4 @@ import {Session} from './session';
 export type Round = {
     title: string;
     sessions: Session[];
-}
+};
--- src/domain/session.ts
+++ src/domain/session.ts
@@ -1,4 +1,4 @@
 export type Session = {
     title: string;
     date: Date;
-}
+};
