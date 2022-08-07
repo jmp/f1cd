7dafde6861224d729bd23b9ac70f7797c4b07916
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-08-07 10:31:09 +0300

    Simplify app tests

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -6,20 +6,24 @@ import {Round} from '../models/round';
 import {Season} from '../models/season';
 
 describe('App', () => {
-    const season = new Season([
-        new Round(
-            'Test round',
-            new Date('2022-01-07T12:00:00Z'),
-            [
-                new Session('First session', new Date('2022-01-01T12:00:00Z')),
-                new Session('Second session', new Date('2022-01-07T12:00:00Z')),
-                new Session('Third session', new Date('2022-01-14T12:00:00Z'))
-            ]
-        )
-    ]);
+    const defaultProps = {
+        season: new Season([
+            new Round(
+                'Test round',
+                new Date('2022-01-07T12:00:00Z'),
+                [
+                    new Session('First session', new Date('2022-01-01T12:00:00Z')),
+                    new Session('Second session', new Date('2022-01-07T12:00:00Z')),
+                    new Session('Third session', new Date('2022-01-14T12:00:00Z'))
+                ]
+            )
+        ]),
+        getDate: () => new Date(),
+        updateInterval: 0
+    };
 
     it('shows header', () => {
-        render(<App season={season} getDate={() => new Date()} updateInterval={0} />);
+        render(<App {...defaultProps} />);
 
         const header = screen.getByTestId('header');
 
@@ -27,7 +31,7 @@ describe('App', () => {
     });
 
     it('shows round info', () => {
-        render(<App season={season} getDate={() => new Date()} updateInterval={0} />);
+        render(<App {...defaultProps} />);
 
         const roundInfo = screen.getByTestId('round-info');
 
@@ -35,7 +39,7 @@ describe('App', () => {
     })
 
     it('shows session info', () => {
-        render(<App season={season} getDate={() => new Date()} updateInterval={0} />);
+        render(<App {...defaultProps} />);
 
         const sessionInfo = screen.getByTestId('session-info');
 
@@ -45,7 +49,7 @@ describe('App', () => {
     it('updates countdown', async () => {
         const mockGetDate = jest.fn().mockReturnValue(new Date('2022-01-06T09:56:56Z'));
 
-        render(<App season={season} getDate={mockGetDate} updateInterval={0} />);
+        render(<App {...defaultProps} getDate={mockGetDate} />);
 
         const element = screen.getByTestId('countdown');
         const previousContent = element.textContent!;
