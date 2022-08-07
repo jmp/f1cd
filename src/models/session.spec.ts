e1fadcd9eae05d55c8318d4cb57a4a21b43a33c4
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-08-07 14:57:15 +0300

    Rename remaining time tests

--- src/models/session.spec.ts
+++ src/models/session.spec.ts
@@ -2,7 +2,7 @@ import {Session} from './session';
 
 describe('session', () => {
     describe('remaining time to the beginning of the session', () => {
-        it('shows zero seconds when there is no time remaining', () => {
+        it('is zero seconds when there is no time remaining', () => {
             const session = new Session('Test session', new Date('2022-01-01T00:00:00Z'));
 
             const remainingTime = session.getRemainingTime(new Date('2022-01-01T00:00:00Z'));
@@ -10,7 +10,7 @@ describe('session', () => {
             expect(remainingTime).toEqual('0 seconds');
         });
 
-        it('shows zero seconds when the session has already begun', () => {
+        it('is zero seconds when the session has already begun', () => {
             const session = new Session('Test session', new Date('2022-01-01T00:00:00Z'));
 
             const remainingTime = session.getRemainingTime(new Date('2022-01-01T00:00:30Z'));
@@ -18,7 +18,7 @@ describe('session', () => {
             expect(remainingTime).toEqual('0 seconds');
         });
 
-        it('shows seconds when the remaining time is less than a minute', () => {
+        it('is seconds when the remaining time is less than a minute', () => {
             const session = new Session('Test session', new Date('2022-01-01T00:00:05Z'));
 
             const remainingTime = session.getRemainingTime(new Date('2022-01-01T00:00:00Z'));
@@ -26,7 +26,7 @@ describe('session', () => {
             expect(remainingTime).toEqual('5 seconds');
         });
 
-        it('shows minutes and seconds when the remaining time is greater than a minute', () => {
+        it('is minutes and seconds when the remaining time is greater than a minute', () => {
             const session = new Session('Test session', new Date('2022-01-01T00:04:05Z'));
 
             const remainingTime = session.getRemainingTime(new Date('2022-01-01T00:00:00Z'));
@@ -34,7 +34,7 @@ describe('session', () => {
             expect(remainingTime).toEqual('4 minutes 5 seconds');
         });
 
-        it('shows hours, minutes and seconds when the remaining time is greater than an hour', () => {
+        it('is hours, minutes and seconds when the remaining time is greater than an hour', () => {
             const session = new Session('Test session', new Date('2022-01-01T03:04:05Z'));
 
             const remainingTime = session.getRemainingTime(new Date('2022-01-01T00:00:00Z'));
@@ -42,7 +42,7 @@ describe('session', () => {
             expect(remainingTime).toEqual('3 hours 4 minutes 5 seconds');
         });
 
-        it('shows days, hours, minutes and seconds when the remaining time is greater than a day', () => {
+        it('is days, hours, minutes and seconds when the remaining time is greater than a day', () => {
             const session = new Session('Test session', new Date('2022-01-03T03:04:05Z'));
 
             const remainingTime = session.getRemainingTime(new Date('2022-01-01T00:00:00Z'));
@@ -50,7 +50,7 @@ describe('session', () => {
             expect(remainingTime).toEqual('2 days 3 hours 4 minutes 5 seconds');
         });
 
-        it('shows seconds in singular when there is exactly one second', () => {
+        it('is seconds in singular when there is exactly one second', () => {
             const session = new Session('Test session', new Date('2022-01-01T00:00:01Z'));
 
             const remainingTime = session.getRemainingTime(new Date('2022-01-01T00:00:00Z'));
@@ -58,7 +58,7 @@ describe('session', () => {
             expect(remainingTime).toEqual('1 second');
         });
 
-        it('shows minutes in singular when there is exactly one minute', () => {
+        it('is minutes in singular when there is exactly one minute', () => {
             const session = new Session('Test session', new Date('2022-01-01T00:01:00Z'));
 
             const remainingTime = session.getRemainingTime(new Date('2022-01-01T00:00:00Z'));
@@ -66,7 +66,7 @@ describe('session', () => {
             expect(remainingTime).toEqual('1 minute 0 seconds');
         });
 
-        it('shows hours in singular when there is exactly one hour', () => {
+        it('is hours in singular when there is exactly one hour', () => {
             const session = new Session('Test session', new Date('2022-01-01T01:00:00Z'));
 
             const remainingTime = session.getRemainingTime(new Date('2022-01-01T00:00:00Z'));
@@ -74,7 +74,7 @@ describe('session', () => {
             expect(remainingTime).toEqual('1 hour 0 minutes 0 seconds');
         });
 
-        it('shows days in singular when there is exactly one day', () => {
+        it('is days in singular when there is exactly one day', () => {
             const session = new Session('Test session', new Date('2022-01-02T00:00:00Z'));
 
             const remainingTime = session.getRemainingTime(new Date('2022-01-01T00:00:00Z'));
