f25ef51c814c611e3a5ce1c66ba6ee45940876d8
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-08-07 10:48:54 +0300

    Add another describe block to be more descriptive

--- src/models/round.spec.ts
+++ src/models/round.spec.ts
@@ -1,37 +1,39 @@
 import {Session} from './session';
 import {Round} from './round';
 
-describe('find next session', () => {
-    it('finds the next session when there are upcoming sessions', () => {
-        const round = new Round(
-            'Test round',
-            new Date('2022-01-01T12:00:00Z'),
-            [
-                new Session('Wrong session', new Date('2022-01-01T12:00:00Z')),
-                new Session('Correct session', new Date('2022-01-07T12:00:00Z')),
-                new Session('Wrong session', new Date('2022-01-14T12:00:00Z'))
-            ]
-        );
-        const nextSession = round.findNextSession(new Date('2022-01-05T12:00:00Z'));
+describe('round', () => {
+    describe('finding the next session', () => {
+        it('finds the next session when there are upcoming sessions', () => {
+            const round = new Round(
+                'Test round',
+                new Date('2022-01-01T12:00:00Z'),
+                [
+                    new Session('Wrong session', new Date('2022-01-01T12:00:00Z')),
+                    new Session('Correct session', new Date('2022-01-07T12:00:00Z')),
+                    new Session('Wrong session', new Date('2022-01-14T12:00:00Z'))
+                ]
+            );
+            const nextSession = round.findNextSession(new Date('2022-01-05T12:00:00Z'));
 
-        expect(nextSession).toEqual(
-            new Session('Correct session', new Date('2022-01-07T12:00:00Z'))
-        );
-    });
-
-    it('finds the last session when there are no upcoming sessions', () => {
-        const round = new Round(
-            'Test session',
-            new Date('2022-01-01T12:00:00Z'),
-            [
-                new Session('Wrong session', new Date('2022-01-01T12:00:00Z')),
+            expect(nextSession).toEqual(
                 new Session('Correct session', new Date('2022-01-07T12:00:00Z'))
-            ]
-        );
-        const nextSession = round.findNextSession(new Date('2022-01-14T12:00:00Z'));
+            );
+        });
 
-        expect(nextSession).toEqual(
-            new Session('Correct session', new Date('2022-01-07T12:00:00Z'))
-        );
+        it('finds the last session when there are no upcoming sessions', () => {
+            const round = new Round(
+                'Test session',
+                new Date('2022-01-01T12:00:00Z'),
+                [
+                    new Session('Wrong session', new Date('2022-01-01T12:00:00Z')),
+                    new Session('Correct session', new Date('2022-01-07T12:00:00Z'))
+                ]
+            );
+            const nextSession = round.findNextSession(new Date('2022-01-14T12:00:00Z'));
+
+            expect(nextSession).toEqual(
+                new Session('Correct session', new Date('2022-01-07T12:00:00Z'))
+            );
+        });
     });
 });
