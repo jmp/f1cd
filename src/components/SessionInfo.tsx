c8a7cb9c6ba7874a249366f6a925a7a5097ba457
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-08-07 11:55:29 +0300

    Make countdown look a bit nicer

--- src/components/SessionInfo.tsx
+++ src/components/SessionInfo.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Session} from '../models/session';
+import './SessionInfo.css';
 
 type CountdownProps = {
     session: Session;
@@ -11,7 +12,10 @@ export function SessionInfo({session, date}: CountdownProps) {
     return (
         <div data-testid='session-info'>
             <h3 data-testid='session-info-heading'>Next Session</h3>
-            <p><span data-testid='session-title'>{session.title}</span> starts in <span data-testid='countdown'>{remainingTime}</span>.</p>
+            <div className='session'>
+                <div data-testid='session-title' className='session-title'>{session.title}</div>
+                <div data-testid='countdown' className='countdown'>{remainingTime}</div>
+            </div>
         </div>
     );
 }
