acfe25c6f5325ddd3809ddc92647a10c4f8c18cd
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-08-08 19:25:22 +0300

    Rename "Grand Prix" to "Race" per official website

--- src/data/season-data.spec.ts
+++ src/data/season-data.spec.ts
@@ -13,7 +13,7 @@ describe('season data mapping', () => {
                 { title: 'Practice 2', date: '2022-07-29T15:00:00Z' },
                 { title: 'Practice 3', date: '2022-07-30T11:00:00Z' },
                 { title: 'Qualifying', date: '2022-07-30T14:00:00Z' },
-                { title: 'Grand Prix', date: '2022-07-31T13:00:00Z' }
+                { title: 'Race', date: '2022-07-31T13:00:00Z' }
             ]
         }];
 
@@ -28,7 +28,7 @@ describe('season data mapping', () => {
                 new Session('Practice 2', new Date('2022-07-29T15:00:00Z')),
                 new Session('Practice 3', new Date('2022-07-30T11:00:00Z')),
                 new Session('Qualifying', new Date('2022-07-30T14:00:00Z')),
-                new Session('Grand Prix', new Date('2022-07-31T13:00:00Z'))
+                new Session('Race', new Date('2022-07-31T13:00:00Z'))
             ]
         )]));
     });
