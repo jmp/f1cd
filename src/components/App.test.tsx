af7343a86055e0749483a53595da260b27d99d99
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-08-09 21:33:40 +0300

    Simplify countdown test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import {render, screen, waitFor} from '@testing-library/react';
 import App from './App';
-import {aRound} from '../models/round.builder';
 import {aSeason} from '../models/season.builder';
-import {aSession} from '../models/session.builder';
 
 describe('app', () => {
     const defaultProps = {
@@ -31,17 +29,14 @@ describe('app', () => {
     });
 
     it('updates countdown', async () => {
-        const season = aSeason()
-            .round(aRound().session(aSession().date(new Date('2022-01-07T00:00Z'))))
-            .build();
-        const mockGetDate = jest.fn().mockReturnValue(new Date('2022-01-06T09:56:56Z'));
+        const getDate = jest.fn().mockReturnValue(new Date('2000-01-01T00:00:00Z'));
 
-        render(<App {...defaultProps} season={season} getDate={mockGetDate} />);
+        render(<App {...defaultProps} season={aSeason().build()} getDate={getDate} />);
 
         const remainingTime = screen.getByTestId('remaining-time');
         const previousContent = remainingTime.textContent!;
 
-        mockGetDate.mockReturnValue(new Date('2022-01-06T09:56:57Z'));
+        getDate.mockReturnValue(new Date('2000-01-01T00:00:01Z'));
 
         await waitFor(() => {
             expect(remainingTime).not.toHaveTextContent(previousContent);
