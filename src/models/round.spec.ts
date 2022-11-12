0d1659d32a7c527a3dc724ac8ade32e290ee707c
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2022-11-12 23:04:17 +0200

    Simplify round test data

--- src/models/round.spec.ts
+++ src/models/round.spec.ts
@@ -17,25 +17,25 @@ describe('round', () => {
         it('finds the first session after the given date', () => {
             const round = aRound()
                 .session(aSession().date(new Date('2022-01-01T12:00Z')))
-                .session(aSession().title('Correct session').date(new Date('2022-01-07T12:00Z')))
+                .session(aSession().date(new Date('2022-01-07T12:00Z')))
                 .session(aSession().date(new Date('2022-01-14T12:00Z')))
                 .build();
             const nextSession = round.findNextSession(new Date('2022-01-05T12:00Z'));
 
             expect(nextSession).toEqual(
-                new Session('Correct session', new Date('2022-01-07T12:00Z'))
+                new Session(expect.anything(), new Date('2022-01-07T12:00Z'))
             );
         });
 
         it('finds the last session when there are no sessions after the given date', () => {
             const round = aRound()
                 .session(aSession().date(new Date('2022-01-01T12:00Z')))
-                .session(aSession().title('Correct session').date(new Date('2022-01-07T12:00Z')))
+                .session(aSession().date(new Date('2022-01-07T12:00Z')))
                 .build();
             const nextSession = round.findNextSession(new Date('2022-01-14T12:00Z'));
 
             expect(nextSession).toEqual(
-                new Session('Correct session', new Date('2022-01-07T12:00Z'))
+                new Session(expect.anything(), new Date('2022-01-07T12:00Z'))
             );
         });
     });
@@ -52,12 +52,12 @@ describe('round', () => {
 
         it('finds the sessions when the date is after the earliest session', () => {
             const round = aRound()
-                .session(aSession().title('Test session').date(new Date('2022-01-01T12:00Z')))
+                .session(aSession().date(new Date('2022-01-01T12:00Z')))
                 .build();
             const sessions = round.findSessionsBefore(new Date('2022-01-05T13:00Z'));
 
             expect(sessions).toEqual([
-                new Session('Test session', new Date('2022-01-01T12:00Z'))
+                new Session(expect.anything(), new Date('2022-01-01T12:00Z'))
             ]);
         });
     });
@@ -74,12 +74,12 @@ describe('round', () => {
 
         it('finds the sessions when the date is before the last session', () => {
             const round = aRound()
-                .session(aSession().title('Test session').date(new Date('2022-01-01T12:00Z')))
+                .session(aSession().date(new Date('2022-01-01T12:00Z')))
                 .build();
             const sessions = round.findSessionsAfter(new Date('2022-01-01T11:00Z'));
 
             expect(sessions).toEqual([
-                new Session('Test session', new Date('2022-01-01T12:00Z'))
+                new Session(expect.anything(), new Date('2022-01-01T12:00Z'))
             ]);
         });
     });
