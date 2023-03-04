7a613410ef1864e6bad113c21af0e42aba9b5289
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2023-03-04 13:34:10 +0200

    Use constant date for accessibility checks

--- cypress/e2e/accessibility.cy.ts
+++ cypress/e2e/accessibility.cy.ts
@@ -19,9 +19,11 @@ describe('color schemes', () => {
     colorSchemes.forEach(scheme =>
         it(`passes with prefers-color-scheme=${scheme}`, () => {
             setPreferredColorScheme(scheme);
+            cy.clock(new Date('2023-03-03T18:00:00Z'), ['Date']);
 
             cy.visit('/');
             cy.injectAxe();
+
             cy.checkA11y();
         })
     );
@@ -32,10 +34,12 @@ describe('screen sizes', () => {
 
     presets.forEach(preset =>
         it(`passes with ${preset} screen size`, () => {
+            cy.clock(new Date('2023-03-03T18:00:00Z'), ['Date']);
             cy.viewport(preset);
 
             cy.visit('/');
             cy.injectAxe();
+
             cy.checkA11y();
         })
     );
