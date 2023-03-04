795c43f5edb2172e116fbc4acbddabbb999b3848
Jarkko Piiroinen <183207+jmp@users.noreply.github.com>
2023-03-04 13:45:32 +0200

    Use default viewport

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,6 @@ import {defineConfig} from 'cypress';
 export default defineConfig({
     e2e: {
         setupNodeEvents() {},
-        viewportWidth: 1280,
-        viewportHeight: 800,
         baseUrl: 'http://localhost:3000',
         video: false
     },
